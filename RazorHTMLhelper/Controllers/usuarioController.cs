22d7e93105df8b72165efa73413dd703f24b0db5
Gerson Gomez <ger.gomezz56@gmail.com>
2023-04-17 10:20:06 -0600

    Agregar archivos de proyecto.

---
+++ RazorHTMLhelper/Controllers/usuarioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RazorHTMLhelper.Controllers
+{
+    public class usuarioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
